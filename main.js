fd659718b93e62b10a7beaaea55ae005a74d1da2
Chiara Albertario <chiara.albertario@gmail.com>
2024-08-04 11:55:17 -0300

    Creacion de los archivos HTML, CSS Y JS de la segunda preentrega

---
+++ main.js
@@ -0,0 +1,57 @@
+const nodoPadre = document.getElementById("nodoPadre") 
+
+
+const productosForrajeria = [
+    {
+        nombre: "Comida para Perros",
+        descripcion: "Bolsa de 10kg de alimento seco.",
+        precio: 46000
+    },
+    {
+        nombre: "Juguete para Gatos",
+        descripcion: "Ratón de peluche con catnip.",
+        precio: 7500
+    },
+    {
+        nombre: "Pecera de Vidrio",
+        descripcion: "Pecera de 20 litros con tapa.",
+        precio: 30000
+    },
+    {
+        nombre: "Cama para Mascotas",
+        descripcion: "Cama acolchada de 70cm.",
+        precio: 26000
+    },
+    {
+        nombre: "Correa Extensible",
+        descripcion: "Correa de 5 metros para perros.",
+        precio: 15200
+    },
+    {
+        nombre: "Comedero Automático",
+        descripcion: "Dispensador automático de comida.",
+        precio: 40000
+    },
+    {
+        nombre: "Rascador para Gatos",
+        descripcion: "Rascador de 1 metro de altura.",
+        precio: 50000
+    },
+    {
+        nombre: "Acuario con Filtro",
+        descripcion: "Acuario de 50 litros con filtro.",
+        precio: 120000
+    },
+    {
+        nombre: "Transportadora",
+        descripcion: "Transportadora mediana para mascotas.",
+        precio: 35000
+    },
+    {
+        nombre: "Arena para Gatos",
+        descripcion: "Bolsa de 5kg de arena.",
+        precio: 10000
+    }
+]
+
+let carrito = []
