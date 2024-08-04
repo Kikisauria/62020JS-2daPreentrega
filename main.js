4ee730fe67e71c5df3752a56ebb3bf0aaa5363cd
Chiara Albertario <chiara.albertario@gmail.com>
2024-08-04 12:42:21 -0300

    Lista de productos forrajeria agregada

--- main.js
+++ main.js
@@ -1,53 +1,64 @@
-const nodoPadre = document.getElementById("nodoPadre") 
+const nodoPadre = document.getElementById("nodoPadre")
 
 
 const productosForrajeria = [
     {
+        img: "",
         nombre: "Comida para Perros",
         descripcion: "Bolsa de 10kg de alimento seco.",
         precio: 46000
     },
+
     {
+        img: "",
         nombre: "Juguete para Gatos",
         descripcion: "Ratón de peluche con catnip.",
         precio: 7500
     },
     {
+        img: "",
         nombre: "Pecera de Vidrio",
         descripcion: "Pecera de 20 litros con tapa.",
         precio: 30000
     },
     {
+        img: "",
         nombre: "Cama para Mascotas",
         descripcion: "Cama acolchada de 70cm.",
         precio: 26000
     },
     {
+        img: "",
         nombre: "Correa Extensible",
         descripcion: "Correa de 5 metros para perros.",
         precio: 15200
     },
     {
+        img: "",
         nombre: "Comedero Automático",
         descripcion: "Dispensador automático de comida.",
         precio: 40000
     },
     {
+        img: "",
         nombre: "Rascador para Gatos",
         descripcion: "Rascador de 1 metro de altura.",
         precio: 50000
     },
     {
+        img: "",
         nombre: "Acuario con Filtro",
         descripcion: "Acuario de 50 litros con filtro.",
         precio: 120000
     },
     {
+        img: "",
         nombre: "Transportadora",
         descripcion: "Transportadora mediana para mascotas.",
         precio: 35000
     },
     {
+        img: "",
         nombre: "Arena para Gatos",
         descripcion: "Bolsa de 5kg de arena.",
         precio: 10000
@@ -55,3 +66,12 @@ const productosForrajeria = [
 ]
 
 let carrito = []
+
+productosForrajeria.forEach((el) => {
+    nodoPadre.innerHTML += `<div>
+<h2>${el.nombre}</h2>
+<img src="${el.img}"/>
+<p>${el.descripcion}</p>
+<p>${el.precio}</p>
+</div>`
+})
