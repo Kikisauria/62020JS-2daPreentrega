33bca73c337fd34d960e28b9a287dedba18f2c0d
Chiara Albertario <chiara.albertario@gmail.com>
2024-08-08 17:08:00 -0300

    agregando imagenes y acomodando un poco el css

--- js/main.js
+++ js/main.js
@@ -10,62 +10,62 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const productosForrajeria = [
     {
-        img: "",
+        img: "https://cdnx.jumpseller.com/pet-bj/image/30072338/seco_perro_cachorro-min.png?1670389327",
         nombre: "Comida para Perros",
         descripcion: "Bolsa de 10kg de alimento seco.",
         precio: 46000
     },
 
     {
-        img: "",
+        img: "https://dojiw2m9tvv09.cloudfront.net/23843/product/M_img-20240404-1045534675.png?49&time=1721093639",
         nombre: "Juguete para Gatos",
         descripcion: "Ratón de peluche con catnip.",
         precio: 7500
     },
     {
-        img: "",
+        img: "https://aquatropical.net/wp-content/uploads/2021/01/boyu-Ms_1.png",
         nombre: "Pecera de Vidrio",
         descripcion: "Pecera de 20 litros con tapa.",
         precio: 30000
     },
     {
-        img: "",
+        img: "https://www.relan.cl/cdn/shop/products/pagina-web-20-60384b4f-05f8-4f2b-a08a-2ca49fd905a2.png?v=1669343581",
         nombre: "Cama para Mascotas",
-        descripcion: "Cama acolchada de 70cm.",
-        precio: 26000
+        descripcion: "Cama acolchada de 90cm.",
+        precio: 58000
     },
     {
-        img: "",
+        img: "https://static.miscota.com/media/1/photos/products/424561/correa-flexi-black-design-m-cordon_1_g.png",
         nombre: "Correa Extensible",
         descripcion: "Correa de 5 metros para perros.",
         precio: 15200
     },
     {
-        img: "",
+        img: "https://feriaonline.co/cdn/shop/files/Comederoautomaticoparamascota8.png?v=1716496483",
         nombre: "Comedero Automático",
-        descripcion: "Dispensador automático de comida.",
-        precio: 40000
+        descripcion: "Dispensador automático de comida para perros y gatos.",
+        precio: 45000
     },
     {
-        img: "",
-        nombre: "Rascador para Gatos",
-        descripcion: "Rascador de 1 metro de altura.",
-        precio: 50000
+        img: "https://maderas86.com/wp-content/uploads/2023/06/mueble-arbol-trepador-rascador-para-gato-con-niveles.png",
+        nombre: "Cama con rascador para Gatos",
+        descripcion: "Mueble de un metro de altura.",
+        precio: 950000
     },
     {
-        img: "",
-        nombre: "Acuario con Filtro",
-        descripcion: "Acuario de 50 litros con filtro.",
-        precio: 120000
+        img: "https://casaluna.com.mx/cdn/shop/files/ID_5037_COD_11093_3322018110937_ORTAUNASPERROCHICOOGATONUNBELL_1200x1200.png?v=1684174126",
+        nombre: "Cortauñas para mascotas",
+        descripcion: "Cortauñas para perros y gatos con lima incluida",
+        precio: 12000
     },
     {
-        img: "",
+        img: "https://i5.walmartimages.com.mx/mg/gm/3pp/asr/c791c0b1-d73b-4b49-bdde-1929e493d53a.167209c3cb3b687c96cda6318c6fe78e.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
         nombre: "Transportadora",
         descripcion: "Transportadora mediana para mascotas.",
         precio: 35000
     },
     {
-        img: "",
+        img: "https://cdn.shopify.com/s/files/1/0566/4391/1854/products/7502279230018_cdcb231a-b9e9-437a-8dc0-9f32b109e06c.png?v=1626656059",
         nombre: "Arena para Gatos",
         descripcion: "Bolsa de 5kg de arena.",
         precio: 10000
@@ -77,10 +77,14 @@ let carrito = []
 productosForrajeria.forEach((el) => {
     nodoPadre.innerHTML += `<div class="producto">
 <h2>${el.nombre}</h2>
-<img src="${el.img}"/>
+<img class="imagenProducto" src="${el.img}"/>
 <p class="descipcion">${el.descripcion}</p>
 <p class="precio">${"$"+el.precio}</p>
 <button type="button" class="boton">Agregar al carrito</button>
 </div>`
 })
 
+
+
+// HACER LOCALSTORAGE Y FUNCION DE AGREGAR/SUMAR/ELIMINAR DEL CARRITO
+//BUSCAR COMO HACERLO MODAL
